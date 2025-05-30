6cf5d9b1581d697856201a82e886b2723bb9d017
Natanael Silva Oliveira <natanaeloliveira2387@gmail.com>
2025-05-30 10:53:22 -0300

    att

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import ee
 import geemap.foliumap as geemap 
-from streamlit_folium import folium_static
+import folium
 from datetime import date, timedelta
 import json
 import tempfile
@@ -25,39 +25,7 @@ st.set_page_config(layout='wide',
 
 st.sidebar.title('Menu')
 
-# datain = st.sidebar.date_input("Data Inicial",  max_value= date.today() - timedelta(days=62))
-# datafi = st.sidebar.date_input("Data Final", max_value= date.today() - timedelta(days=61))
-
-# # Converte as datas para strings no formato 'YYYY-MM-DD'
-# datain_str = datain.strftime('%Y-%m-%d')
-# datafi_str = datafi.strftime('%Y-%m-%d')
-
-# # Carrega dados CHIRPS
-# chirps = ee.ImageCollection('UCSB-CHG/CHIRPS/DAILY') \
-#            .select('precipitation') \
-#            .filterDate(datain_str, datafi_str)
-           
-
-
-# # Paleta de cores
-# vis = {
-#     'min': 0,
-#     'max': 100,
-#     'palette': ['000080', '0000d9', '4000ff', '8000ff', '0080ff', '00ffff', '00ff80',
-#                 '80ff00', 'daff00', 'ffff00', 'fff500', 'ffda00', 'ffb000', 'ffa400',
-#                 'ff4f00', 'ff2500', 'ff0a00', 'ff00ff']
-# }
-
-# # Cria o mapa
-# Map = geemap.Map(center=[-19, -60], zoom=4, tiles='cartodbdark_matter')
-# Map.addLayer(chirps.sum(), vis, 'Precipitação - 2024')
-# Map.add_colorbar(vis, label='Precipitação [mm/ano]')
-#Map.to_streamlit(width=1500, height=700)
-
-
-
-
-datafi = st.sidebar.date_input("Data", max_value= date.today())
+datafi = st.sidebar.date_input("Data", max_value= date.today() -  timedelta(days=2))
 
 
 # # Converte as datas para strings no formato 'YYYY-MM-DD'
@@ -68,24 +36,23 @@ datafi_str = datafi.strftime('%Y-%m-%d')
 
 
 # Carrega o dataset GPM
-dataset = ee.ImageCollection('JAXA/GPM_L3/GSMaP/v8/operational') \
+dataset = ee.ImageCollection('NASA/GPM_L3/IMERG_V07') \
     .filter(ee.Filter.date(datain_str, datafi_str))
     
 
 
 # Seleciona a taxa de precipitação horária
-precipitation = dataset.select('hourlyPrecipRate')
+precipitation = dataset.select('precipitation')
 
 # Configura a visualização
 precipitationVis = {
-    'min': 2,
+    'min': 1,
     'max': 150.0,
     'palette': ['1621a2','03ffff', '13ff03', 'efff00', 'ffb103', 'ff2300']}
 
 # Cria o mapa
 Map = geemap.Map(center=[-19, -60], zoom=4, tiles='cartodbdark_matter')
-Map.addLayer(precipitation.sum().updateMask(precipitation.sum().gt(2)), precipitationVis, 'Precipitação Horária', opacity=1)
-
+Map.addLayer(precipitation.sum().updateMask(precipitation.sum().gt(0.5)), precipitationVis, 'Precipitação Horária', opacity=1)
 
 Map.add_colorbar(precipitationVis, background_color='white', step= 20, label='Precipitação [mm/h]')
 Map.to_streamlit(width=1820, height=900)
